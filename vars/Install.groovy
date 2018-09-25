3effac87088baa3aebe0c1fffbbfb33a0eea7ead
shreya1396 <42531964+shreya1396@users.noreply.github.com>
2018-09-25 12:28:51 +0530

    Update Install.groovy

--- vars/Install.groovy
+++ vars/Install.groovy
@@ -1,1 +1,26 @@
+def call(body) {
 
+        def config = [:]
+        body.resolveStrategy = Closure.DELEGATE_FIRST
+        body.delegate = config
+        body()
+
+        node {
+            // Clean workspace before doing anything
+            deleteDir()
+
+            try {
+                stage ('Build') {
+                    sh '''
+                 sudo apt-get update
+            	   sudo apt-get install -y make cmake gcc g++ wget tar git curl
+            	   sudo apt-get install -y golang-1.10
+                 '''
+                }
+              
+              } catch (err) {
+                currentBuild.result = 'FAILED'
+                throw err
+            }
+        }
+    }
