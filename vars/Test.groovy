2ad8f9a744f70f38a02e763f7cbedbc5db8220d6
shreya1396 <42531964+shreya1396@users.noreply.github.com>
2018-09-25 12:37:03 +0530

    Update Test.groovy

--- vars/Test.groovy
+++ vars/Test.groovy
@@ -1,1 +1,25 @@
+def call(body) {
 
+        def config = [:]
+        body.resolveStrategy = Closure.DELEGATE_FIRST
+        body.delegate = config
+        body()
+
+        node {
+            // Clean workspace before doing anything
+            deleteDir()
+
+            try {
+                stage ('Build') {
+                    sh '''
+                    sed -i 's/$(GO) test -race $(pkgs)/$(GO) test $(pkgs)/' Mekefile.common
+                    make test
+                 '''
+                }
+              
+              } catch (err) {
+                currentBuild.result = 'FAILED'
+                throw err
+            }
+        }
+    }
