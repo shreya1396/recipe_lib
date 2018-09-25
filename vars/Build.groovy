ace3ffa0116bf9e030cfd07a9f421789244f59a6
shreya1396 <42531964+shreya1396@users.noreply.github.com>
2018-09-25 12:26:02 +0530

    Update Build.groovy

--- vars/Build.groovy
+++ vars/Build.groovy
@@ -1,1 +1,29 @@
+def call(body) {
 
+        def config = [:]
+        body.resolveStrategy = Closure.DELEGATE_FIRST
+        body.delegate = config
+        body()
+
+        node {
+            // Clean workspace before doing anything
+            deleteDir()
+
+            try {
+                stage ('Build') {
+                    sh '''
+                 mkdir -p $GOPATH/src/github.com/prometheus
+                 cd $GOPATH/src/github.com/prometheus
+                 git clone https://github.com/prometheus/prometheus.git
+                 cd prometheus
+                 git checkout v2.3.2
+                 make build
+                 '''
+                }
+              
+              } catch (err) {
+                currentBuild.result = 'FAILED'
+                throw err
+            }
+        }
+    }
